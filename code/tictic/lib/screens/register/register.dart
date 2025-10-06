444a0a5cddeb79648c949619aa99a64528bec879
BriolVictoria <victoria.briol@student.hepl.be>
2025-10-06 16:52:13 +0200

    Add register page

--- code/tictic/lib/screens/register/register.dart
+++ code/tictic/lib/screens/register/register.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tictic/constants/sizes.dart';
+import 'package:tictic/screens/scaffold_with_image.dart';
 
 class Register extends StatelessWidget {
   const Register({super.key});
+
   static const String routeName = '/register';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder());
+    return ScaffoldWithImage(
+      child: Padding(
+        padding: const EdgeInsets.only(top: kVerticalPadding * 2),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(width: kHorizontalPadding),
+                ElevatedButton(onPressed: () {
+                  Navigator.pop(context);
+                }, child: Icon(Icons.arrow_back)),
+              ],
+            ),
+            Text('Register'),
+          ],
+        ),
+      ),
+    );
   }
 }
